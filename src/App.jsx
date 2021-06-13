bb292d6fa188b9136ea0fc958ec304fcdcd70914
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-06-13 18:50:38 -0300

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+// Generamos la función del compoente
+const App = ()=> {
+    // Lo que ejecuta la funcion 
+    return (<div>
+        Calc app
+
+    </div>)
+
+}
+
+//Exportación 
+
+export default App
