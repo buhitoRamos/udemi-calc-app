bfccf3bc0b88335014fa1955b9edb91ed6168531
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-07-08 18:45:24 -0300

    feat: rename component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './Button.css'
+//podemos eliminar las llaves y return cuando la funcion solo tiene un retorno, se puede quitar return 
+// y dejar solo los parentesis
+
+const Button = ({ text, type }) => {
+  function clickHandler(txt) {
+    console.log(text)
+  }
+  return (
+    <button className={type} onClick={() => clickHandler(text)}>
+      <span>{text}</span>
+    </button>
+  )
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  text: PropTypes.string.isRequired,
+  // clickHandler: PropTypes.func.isRequired
+}
+export default Button
