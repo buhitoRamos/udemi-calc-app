ef10044788bb34e635f7a9c5a278046807a730d7
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-07-08 18:42:24 -0300

    feat: replacement buttons by components

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,35 @@
 import React from 'react'
-import Button from './components/Button'
+import Functions from './components/Functions'
+import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
 import './App.css'
 import Result from './components/Result'
 
 // Generamos la función del compoente
-const App = ()=> {
-    const clickHandler = text => {
-        console.log(text)
-    }
-    // Lo que ejecuta la funcion 
-    return (
-    <main className='react-calculator'>
-        <Result value={'10'}/>
-        <div className="numbers">
-            <Button text="1" clickHandler={clickHandler}/>
-            <Button text="2" clickHandler={clickHandler}/>
-            <Button text="3" clickHandler={clickHandler}/>
-            <Button text="4" clickHandler={clickHandler}/>
-            <Button text="5" clickHandler={clickHandler}/>
-            <Button text="6" clickHandler={clickHandler}/>
-            <Button text="7" clickHandler={clickHandler}/>
-            <Button text="8" clickHandler={clickHandler}/>
-            <Button text="9" clickHandler={clickHandler}/>
-            <Button text="0" clickHandler={clickHandler}/>
-        </div>
-        <div className="functions">
-            <button>clear</button>
-            <button>r</button>
-        </div>
-        <div className="math-operations">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>/</button>
-            <button>=</button>
-        </div>
-       
+const App = () => {
 
+  const onClickNumbers = text => {
+    console.log(text)
+  }
+  const onClickOperation = operation => {
+    console.log(operation)
+  }
+  const onClickEqual = equals => {
+    console.log(equals)
+  }
+  // Lo que ejecuta la funcion 
+  return (
+    <main className='react-calculator'>
+      <Result value={'10'} />
+      <Numbers clickHandler={onClickNumbers} />
+      <Functions
+        onContentClear={(text) => console.log('functionsClear:' + text)}
+        onDelete={(text) => console.log('functionsDelete:' + text)}
+      />
+      <MathOperations
+        onClickOperation={onClickOperation}
+        onClickEqual={onClickEqual}
+      />
     </main>)
 
 }
