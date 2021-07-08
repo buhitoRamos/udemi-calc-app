33cb95887f4665ccbdc8b08efafc25ec523cacac
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-07-08 18:44:36 -0300

    feat: fix lint

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,21 +3,19 @@ import React from 'react'
 // Esta libreria ayuda resuelve muchos problemas que se dan cuando se envian parametros de tipos invalidos u se omiten.
 import PropTypes from 'prop-types'
 
-const Result = ({value}) => {
-    return(
-        <div className="result">
-            {value}
-        </div>
-    )
-}
+const Result = ({ value }) => (
+  <div className="result">
+    {value}
+  </div>
+)
 
 //Con esto verifico que values sea String y que llegue.
 Result.propTypes = {
-    value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired
 }
 
 //Con esto en caso que no llegue ningun valor se le asignara 0 a la propiedad.
 Result.defaultProps = {
-    value:'0'
+  value: '0'
 }
 export default Result
