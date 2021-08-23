b5d4e73140eeb9f804d261b3d6188bd5830bbf31
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-08-22 22:47:17 -0300

    feat(Functions.jsx): add new button

--- src/components/Functions.jsx
+++ src/components/Functions.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Functions = ({ onContentClear, onDelete }) => (
+const Functions = ({ onContentClear, onDelete, showHistory }) => (
   <section className="functions">
-    <Button type='button-long-text' text="clear" clickHandler={onContentClear} />
-    <Button text="r" clickHandler={onDelete} />
+    <Button type='button-long-text' text="Clear" clickHandler={onContentClear} />
+    <Button type='button-long-text' text="→" clickHandler={onDelete} />
+    <Button type='button-long-text' text="History" clickHandler={showHistory} />
   </section>
 )
 Functions.propTypes = {
