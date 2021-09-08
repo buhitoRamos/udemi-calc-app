7ef73f9a254b039f5305b56166d308afcf82897f
martin.lopez3.contractor.gdevtools.com <martin.lopez3.contractor@gdevtools.com>
2021-09-07 21:43:20 -0300

    feat(Result): add number separation and operators

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Result = ({ value, clasType }) => (
-  <div className={clasType}>
-    {value}
+const Result = ({ value, classType, isHistory }) => {
+
+ let newValue = value;
+ let matOp = ""
+
+  if (isHistory) {
+    matOp = newValue[value.length - 1];
+    if (matOp === '-' || matOp === '+' || matOp === '*' || matOp === '/') {
+      newValue = newValue.substring(0, newValue.length - 1);
+    } else {
+      matOp = "";
+    }
+  }
+
+  return (
+    <div className={classType}>
+     <span>{newValue}</span> 
+     <span>{matOp}</span>
   </div>
-)
+  )
+}
 
 Result.propTypes = {
-  value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired,
+  classType: PropTypes.string.isRequired,
 }
 
 Result.defaultProps = {
-  value: '0'
+  value: '0',
 }
 export default Result
